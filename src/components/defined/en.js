5b4581aeeb88402b4986a17410ee5c07f3ccfe94
조현재 <johyeonjae@johyeonjaejaeui-MacBookPro.local>
2021-01-31 16:15:33 +0900

    feat: i18n settings - i18n settings

--- src/components/defined/en.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export const en = {
-  initial1: `나의 생각이 전세계에${'\n'}반영되는 설문조사`,
-  initial2: `쉽고 안전하고 빠른 지갑 송금${'\n'}Real Research에서 가능합니다.`,
-  initial3: `Real Research 만의${'\n'}wow Reward 지금 시작하세요!`,
-  confirm: 'Confrim',
-  cancel: 'Cancel',
-};
