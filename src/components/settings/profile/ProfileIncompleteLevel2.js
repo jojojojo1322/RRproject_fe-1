3e9b1f962c34eba6284fc139aa5ee6285e52430f
orbit <orbit@orbitui-MacBookPro.local>
2021-02-03 12:27:07 +0900

    fix: profile incomplete level 2

--- src/components/settings/profile/ProfileIncompleteLevel2.js
+++ src/components/settings/profile/ProfileIncompleteLevel2.js
@@ -1,45 +1,358 @@
-import React, {Component, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  StyleSheet,
   View,
   Text,
-  ScrollView,
-  Button,
   TouchableOpacity,
-  Alert,
   Image,
-  Platform,
   FlatList,
+  StyleSheet,
 } from 'react-native';
-
-import PropTypes from 'prop-types';
 import ResetStyle from '../../../style/ResetStyle.js';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {SelectedCheckboxes, RoundCheckbox} from '../../factory/Roundcheck';
 import ResearchStyle from '../../../style/ResearchStyle.js';
 import ProfileStyle from '../../../style/ProfileStyle.js';
+import AuthStyle from '../../../style/AuthStyle.js';
+import {server} from '../../defined/server';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const ProfileIncompleteLevel2 = () => {
-  const DATA = [
+const ProfileIncompleteLevel2 = (props) => {
+  const QusetionData = [
     {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'First Item',
+      question: 'Waht is your estimated annual revenue?',
+      answers: [
+        {
+          id: 0,
+          answer: 'Under $50,000',
+        },
+        {
+          id: 1,
+          answer: '$50,000 - $100,000',
+        },
+        {
+          id: 2,
+          answer: '$100,000- $249,000',
+        },
+        {
+          id: 3,
+          answer: '$250,000 - $499,999',
+        },
+        {
+          id: 4,
+          answer: '$500,000 - $999,999',
+        },
+        {
+          id: 5,
+          answer: '$1 million - $4.99 million',
+        },
+        {
+          id: 6,
+          answer: '$5 million or more',
+        },
+        {
+          id: 7,
+          answer: 'I don’t work / we don’t work',
+        },
+        {
+          id: 8,
+          answer: 'Prefer not to answer',
+        },
+      ],
     },
     {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Second Item',
+      question: 'What is your current employment status?',
+      answers: [
+        {
+          id: 0,
+          answer: 'Employed full-time',
+        },
+        {
+          id: 1,
+          answer: 'Self-employed full-time',
+        },
+        {
+          id: 2,
+          answer: 'Active military',
+        },
+        {
+          id: 3,
+          answer: 'Temporarily unemployed',
+        },
+        {
+          id: 4,
+          answer: 'Retired',
+        },
+        {
+          id: 5,
+          answer: 'Disabled / Permanently unemployed',
+        },
+        {
+          id: 6,
+          answer: 'Employed part-time',
+        },
+        {
+          id: 7,
+          answer: 'Self-employed part-time',
+        },
+        {
+          id: 8,
+          answer: 'Inactive military / veteran',
+        },
+        {
+          id: 9,
+          answer: 'Full-time homemaker / family dependent',
+        },
+        {
+          id: 10,
+          answer: 'Student',
+        },
+        {
+          id: 11,
+          answer: 'Prefer not to answer',
+        },
+      ],
     },
     {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Third Item',
+      question: 'Where do you invest your income?',
+      answers: [
+        {
+          id: 0,
+          answer: 'Real Estate',
+        },
+        {
+          id: 1,
+          answer: 'Stock Market Investment',
+        },
+        {
+          id: 2,
+          answer: 'Equity Mutual Funds',
+        },
+        {
+          id: 3,
+          answer: 'Cryptocurrencies',
+        },
+        {
+          id: 4,
+          answer: 'Gold',
+        },
+        {
+          id: 5,
+          answer: 'Savings Account (Time Deposit)',
+        },
+        {
+          id: 6,
+          answer: 'Business Capital Investment',
+        },
+        {
+          id: 7,
+          answer: 'Prefer not to answer',
+        },
+      ],
+    },
+    {
+      question: 'Roughly what is your net worth?',
+      answers: [
+        {
+          id: 0,
+          answer: 'Under $50,000',
+        },
+        {
+          id: 1,
+          answer: '$50,000 - $100,000',
+        },
+        {
+          id: 2,
+          answer: '$100,000- $249,000',
+        },
+        {
+          id: 3,
+          answer: '$250,000 - $499,999',
+        },
+        {
+          id: 4,
+          answer: '$500,000 - $999,999',
+        },
+        {
+          id: 5,
+          answer: '$1 million - $4.99 million',
+        },
+        {
+          id: 6,
+          answer: '$5 million - $9.99 million',
+        },
+        {
+          id: 7,
+          answer: '$100 million - $249.99 million',
+        },
+        {
+          id: 8,
+          answer: '$250 million - $499.99 million',
+        },
+        {
+          id: 9,
+          answer: '$500 million - $999.99 million',
+        },
+        {
+          id: 10,
+          answer: '$1 billion or more',
+        },
+        {
+          id: 11,
+          answer: 'Prefer not to answer',
+        },
+      ],
+    },
+    {
+      question: 'Do you own one or more properties?',
+      answers: [
+        {
+          id: 0,
+          answer: 'Yes, I own one house',
+        },
+        {
+          id: 1,
+          answer: 'Yes, I own two houses or more',
+        },
+        {
+          id: 2,
+          answer: 'Yes, I own multiple properties',
+        },
+        {
+          id: 3,
+          answer: 'No',
+        },
+        {
+          id: 4,
+          answer: 'Prefer not to answer',
+        },
+      ],
     },
   ];
 
-  const Item = ({title}) => {
+  // KYC Level 2 Answers
+  const [annualRevenue, setAnnualRevenue] = useState(0);
+  const [employmentStatus, setEmploymentStatus] = useState(0);
+  const [investIn, setInvestIn] = useState(0);
+  const [netWorth, setNetWorth] = useState(0);
+  const [ownProperties, setOwnProperties] = useState(0);
+
+  // Tools for this page
+  const [checkId, setCheckId] = useState('');
+  const [nowIndex, setNowIndex] = useState(0);
+  const [questionLength, setQuestionLength] = useState(0);
+  const [isChecked, setIsChecked] = useState(false);
+  const [checkedArray, setCheckedArray] = useState([]);
+  const [topCheck, setTopCheck] = useState([]);
+
+  useEffect(() => {
+    setQuestionLength(5);
+    // getKycLevel2();
+    console.log(QusetionData[0].answers);
+  }, []);
+
+  const getKycLevel2 = async (
+    annualRevenue,
+    employmentStatus,
+    investIn,
+    netWorth,
+    ownProperties,
+  ) => {
+    const userNo = await AsyncStorage.getItem('userNo');
+    await axios
+      .post(`${server}/kyc/2/`, {
+        annualRevenue: annualRevenue,
+        employmentStatus: employmentStatus,
+        investIn: investIn,
+        netWorth: netWorth,
+        ownProperties: ownProperties,
+        userNo: userNo,
+      })
+      .then(async (response) => {
+        console.log('getKycLevel2 Then>>>', response);
+      })
+      .catch((e) => {
+        console.log('getKycLevel2 Error>>', e);
+      });
+  };
+
+  const handlerPrev = (e) => {
+    e.preventDefault();
+    const _nowIndex = nowIndex;
+    if (_nowIndex != 0) {
+      // setState({
+      //   nowIndex: nowIndex - 1,
+      //   checkId: '',
+      // });
+      setNowIndex(_nowIndex - 1);
+      setCheckId('');
+      // props.navigation.goBack();
+    } else if (_nowIndex == 0) {
+      props.navigation.goBack();
+    }
+  };
+
+  const handlerNext = (e) => {
+    e.preventDefault();
+    const _nowIndex = nowIndex;
+    if (_nowIndex != questionLength - 1) {
+      // setState({
+      //   nowIndex: nowIndex + 1,
+      //   checkId: '',
+      // });
+      setNowIndex(_nowIndex + 1);
+      setCheckId('');
+    }
+    if (_nowIndex === questionLength - 1) {
+      props.navigation.navigate('ProfileComplete');
+    }
+  };
+
+  const Item = (item) => {
     return (
-      <TouchableOpacity>
-        <Text>{title}</Text>
-      </TouchableOpacity>
+      <View
+        style={[
+          ResearchStyle.researchAnswerStyle,
+          ResearchStyle.researchAnswerTopStyle,
+        ]}>
+        <View style={{width: '90%'}}>
+          <Text
+            style={[
+              ResetStyle.fontRegularK,
+              ResetStyle.fontBlack,
+              {textAlign: 'left'},
+            ]}></Text>
+        </View>
+        <RoundCheckbox
+          size={30}
+          // keyValue={item.questionNumber}
+          // checked={
+          //   checkedArray.findIndex(
+          //     (y) =>
+          //       y.question === item.questionNumber &&
+          //       y.answer === item.optionNumber,
+          //   ) >= 0
+          //     ? true
+          //     : false
+          // }
+          color="#164895"
+          labelColor="#000000"
+          // label={item.optionContent}
+          // value={item.optionNumber}
+          onClick={() => {
+            // setState({
+            //   isChecked: !isChecked,
+            //   checkId: item.id,
+            // });
+            // setIsChecked(!isChecked);
+            // setCheckId(item.optionNumber);
+          }}
+          // isChecked={isChecked && checkId == 1}
+          // checkedObjArr={CheckedArrObject}
+          // handleQuestion={handleQuestion}
+          // checkedArray={checkedArray}
+        />
+      </View>
     );
   };
 
@@ -48,7 +361,10 @@ const ProfileIncompleteLevel2 = () => {
       <View
         style={[ResetStyle.containerInner, {marginLeft: 0, marginRight: 0}]}>
         {/* Top */}
-        <View style={{flexDirection: 'column'}}>
+        <View
+          style={{
+            flexDirection: 'column',
+          }}>
           {/* Top Title */}
           <Text
             style={[
@@ -60,31 +376,91 @@ const ProfileIncompleteLevel2 = () => {
           </Text>
 
           {/* 상단 체크박스 */}
-          <View style={[ProfileStyle.incompleteTopView]}></View>
-        </View>
+          <View style={[ProfileStyle.incompleteTopView]}>
+            <Image
+              source={require('../../../imgs/drawable-mdpi/icon_ktit_on.png')}
+            />
+            <View
+              style={{
+                width: 20,
+                borderWidth: 0.5,
+                borderColor: '#dddddd',
+                marginRight: 4,
+                marginLeft: 4,
+              }}
+            />
+            <Image
+              source={require('../../../imgs/drawable-mdpi/icon_ktit_off.png')}
+            />
+            <View
+              style={{
+                width: 20,
+                borderWidth: 0.5,
+                borderColor: '#dddddd',
+                marginRight: 4,
+                marginLeft: 4,
+              }}
+            />
+            <Image
+              source={require('../../../imgs/drawable-mdpi/icon_ktit_off.png')}
+            />
+            <View
+              style={{
+                width: 20,
+                borderWidth: 0.5,
+                borderColor: '#dddddd',
+                marginRight: 4,
+                marginLeft: 4,
+              }}
+            />
+            <Image
+              source={require('../../../imgs/drawable-mdpi/icon_ktit_off.png')}
+            />
+            <View
+              style={{
+                width: 20,
+                borderWidth: 0.5,
+                borderColor: '#dddddd',
+                marginRight: 4,
+                marginLeft: 4,
+              }}
+            />
+            <Image
+              source={require('../../../imgs/drawable-mdpi/icon_ktit_off.png')}
+            />
+          </View>
 
-        {/* Research Form */}
-        <FlatList
-          data={DATA}
-          renderItem={({item}) => <Item title={item.title} />}
-          keyExtractor={(item) => item.id}
-        />
+          {/* Research Form */}
+          <View>
+            <Text
+              style={[
+                ResetStyle.fontRegularK,
+                ResetStyle.fontBlack,
+                ResearchStyle.researchQuestion,
+                {textAlign: 'center'},
+              ]}>
+              {QusetionData[0].question}
+            </Text>
+          </View>
+          <FlatList
+            style={{maxHeight: '70%', height: '50%'}}
+            data={QusetionData[0].answers}
+            renderItem={({item}) => <Item />}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
 
         {/* Bottom Button */}
         <View style={[ResearchStyle.researchBottomButton]}>
           <TouchableOpacity
-            style={[
-              ResetStyle.button,
-              {backgroundColor: '#e6e6e6', width: '49%'},
-            ]}
-            // style={
-            //   this.state.nowIndex == 0
-            //     ? [
-            //         ResetStyle.button,
-            //         {width: '49%', backgroundColor: '#e6e6e6'},
-            //       ]
-            //     : [ResetStyle.button, {width: '49%'}]
-            // }
+            style={
+              nowIndex == 0
+                ? [
+                    ResetStyle.button,
+                    {width: '49%', backgroundColor: '#e6e6e6'},
+                  ]
+                : [ResetStyle.button, {width: '49%'}]
+            }
             activeOpacity={0.75}>
             <Text
               style={[
@@ -92,8 +468,7 @@ const ProfileIncompleteLevel2 = () => {
                 ResetStyle.fontWhite,
                 {fontWeight: '600'},
               ]}>
-              {/* {this.state.nowIndex == 0 ? '취소' : '이전'} */}
-              취소
+              {nowIndex == 0 ? '취소' : '이전'}
             </Text>
           </TouchableOpacity>
           <TouchableOpacity
@@ -111,10 +486,7 @@ const ProfileIncompleteLevel2 = () => {
                 ResetStyle.fontWhite,
                 {fontWeight: '600'},
               ]}>
-              {/* {this.state.nowIndex == this.state.questionLength - 1
-                ? '제출'
-                : '다음'} */}
-              다음
+              {nowIndex == questionLength - 1 ? '제출' : '다음'}
             </Text>
           </TouchableOpacity>
         </View>
--- src/components/settings/profile/ProfileMain.js
+++ src/components/settings/profile/ProfileMain.js
@@ -87,7 +87,7 @@ const ProfileMain = ({navigation}) => {
         onPress={() => {
           level <= kycLevel
             ? navigation.navigate('ProfileCompleteDetail')
-            : level === 2
+            : level === 3
             ? navigation.navigate('ProfileIncompleteLevel2')
             : navigation.navigate('ProfileIncompleteDetail', {
                 KycLevel: level,
